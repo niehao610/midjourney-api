73aa093ba7a148149260accc6bc0119963a3b4c5
forry <forry@jifeng.online>
2025-06-22 23:52:50 +0800

    支持appkey校验2

--- lib/database.py
+++ lib/database.py
@@ -9,11 +9,11 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME", "luban_v2v")
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "12345678")
 
 # 数据库连接字符串
 DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
-
+print("!!!!!!!",DATABASE_URL)
 # 创建数据库连接
 database = Database(DATABASE_URL)
 
